417a053abff9fd6eb4f3bd8f79c967b7e4560930
Liisa Davydov <liisadavydov@gmail.com>
2023-09-10 01:12:21 +0300

    Add chapter 3 folder

---
+++ Viope_JS/Chapter3/3.6/shit.js
@@ -0,0 +1,12 @@
+function calculateUnitFine() {
+    // Read the value from the input field into a variable
+let income = document.getElementById("txtNetIncome").value;	  
+
+    // Calculate the unit fine and assign the result into another variable
+let unitfine = 	(income-255)/60
+
+    // Write the answer to the web page DOM, in the answer div, as the content
+    
+console.log(unitfine);
+document.getElementById("answerDiv").innerHTML = "Unit fine is " + unitfine.toFixed(2);
+}
