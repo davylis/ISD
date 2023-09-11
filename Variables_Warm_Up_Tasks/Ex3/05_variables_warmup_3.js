ccb6c31effd83a368b0f08d1a5b3846011194104
Liisa Davydov <liisadavydov@gmail.com>
2023-09-11 15:53:51 +0300

    Add ex3 to directory

--- Variables_Warm_Up_Tasks/Ex3/05_variables_warmup_3.js
+++ Variables_Warm_Up_Tasks/Ex3/05_variables_warmup_3.js
@@ -1,2 +1,12 @@
 // 05_variables_warmup_3 JavasScript code
-
+// create function
+function showMinutes() {
+// create variable for hours
+    var h = Number(document.getElementById("txtHours").value);
+// create variable for minutes
+    var m = Number(document.getElementById("txtMinutes").value);
+// create variable for changing hours to minutes
+    var htom = h * 60;
+// create variable for the result
+    document.getElementById("pOutput").innerHTML = htom + m + " " + "minutes";
+}
