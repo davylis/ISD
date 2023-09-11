ceea0fcc0a388f7964448e99804997cee4c50281
Liisa Davydov <liisadavydov@gmail.com>
2023-09-11 15:41:44 +0300

    Add ex2 to directory

--- Variables_Warm_Up_Tasks/Ex2/05_variables_warmup_2.js
+++ Variables_Warm_Up_Tasks/Ex2/05_variables_warmup_2.js
@@ -1,2 +1,12 @@
 // 05_variables_warmup_2 JavasScript code
-
+//Add function call
+function add() {
+    // create variable for the first number
+    let txt1 = Number(document.getElementById("txtFirst").value);
+    // create variable for the second number
+    let txt2 = Number(document.getElementById("txtSecond").value);
+    // create variable for the sum
+    let sum = txt1 + txt2
+    // show output on the HTML page
+    document.getElementById("pOutput").innerHTML = txt1 + '+' + txt2 + '=' + sum;
+}
