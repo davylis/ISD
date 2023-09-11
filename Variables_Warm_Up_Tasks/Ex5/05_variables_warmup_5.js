04bd9abc01bd2dc9cfd68da096a8da733d250922
Liisa Davydov <liisadavydov@gmail.com>
2023-09-11 17:10:44 +0300

    Editing the code

--- Variables_Warm_Up_Tasks/Ex5/05_variables_warmup_5.js
+++ Variables_Warm_Up_Tasks/Ex5/05_variables_warmup_5.js
@@ -1,2 +1,13 @@
 // 05_variables_warmup_5 JavasScript code
+function calculate() {
+    var num1 = Number(document.getElementById("txtStartingBalance").value);
+    var num2 = Number(document.getElementById("txtInterestRate").value);
+    var procent = num2 / 100;
+    var year1 = procent * num1 + num1;
+    var year2 = year1 * procent + year1;
+
+    var output = "1. year: " + year1.toFixed(2) + "<br>2. year: " + year2.toFixed(2)
+
+    document.getElementById("pOutput").innerHTML = output;
+}
 
