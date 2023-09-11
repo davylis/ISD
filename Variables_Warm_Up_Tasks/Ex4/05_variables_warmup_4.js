4963127664dc95c605e2cc70cdbd7ee8628c85f2
Liisa Davydov <liisadavydov@gmail.com>
2023-09-11 17:09:34 +0300

    Editing the code

--- Variables_Warm_Up_Tasks/Ex4/05_variables_warmup_4.js
+++ Variables_Warm_Up_Tasks/Ex4/05_variables_warmup_4.js
@@ -1,2 +1,8 @@
 // 05_variables_warmup_4 JavasScript code
-
+function showHoursAndMinutes() {
+    var min = Number(document.getElementById("txtMinutes").value);
+    var mintohour = Math.floor(min / 60); //calculate the whole hours
+    var remainder = min % 60; //calculate the remaining minutes
+    var result = mintohour + " h " + remainder + " min"
+    document.getElementById("pOutput").innerHTML = result;
+}
