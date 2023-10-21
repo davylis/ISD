f650ad2733968a9c7c0a475d59f2151279f79d49
Liisa Davydov <liisamuttonen@gmail.com>
2023-10-21 16:16:38 +0300

    Add viope ex. 7

--- Viope_JS/Chapter6/6.7/09.08LotteryEx.js
+++ Viope_JS/Chapter6/6.7/09.08LotteryEx.js
@@ -2,27 +2,29 @@
 	// The array is availabe for both functions to read from or write to.
 	let names = [];
 
+
 	function addName() {
 		// Assign name from the input field to the array (use push())
-		
-		
+		let name = document.getElementById("name").value
+		names.push(name)
+		let sum = "";
 		// Go through the array in a for loop adding each name at the end of the namesText
-		
+		for(let i=0; i<names.length; i++){
+			sum+=names[i];
+			sum+=" ";
+			
+		}
 		// Write the names on the page, to the names div, as content of the div
-		
-
-		// Empty the input field of name as that name was just put in the array
-
-
-		// Empty the answer text
+		document.getElementById("nameList").innerHTML="Inserted names: " + sum;
 	}
 
 	function makeDraw() {
 
 		// Randomize the index of the winner
-		
+		let randomIndex=Number(Math.floor(Math.random() * names.length));
 		
 		// Write the answer on the page, to the answer div, as content of the div
+		(document.getElementById("answer").innerHTML="<br>Winner is " + names[randomIndex]);
 		
 
 	}
