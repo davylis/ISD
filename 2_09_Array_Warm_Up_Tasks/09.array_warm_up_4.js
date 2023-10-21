6e788be3b61e1d6cb65df6bbd1065dd308e4ca3f
Liisa Davydov <liisamuttonen@gmail.com>
2023-10-22 00:02:23 +0300

    Add Warmup ex.4

--- 2_09_Array_Warm_Up_Tasks/09.array_warm_up_4.js
+++ 2_09_Array_Warm_Up_Tasks/09.array_warm_up_4.js
@@ -1,28 +1,19 @@
-// 09.array_warm_up_2.js JavasScript code
-// Initially, this program can translate only three words (cat, dog, bird).
-// Modify the program so that it can translate more words. 
-// Please feel free to choose the words yourself.
 
-function translateToFinnish() {
-    var givenWord = document.getElementById("txtEnglishWord").value;
-    givenWord = givenWord.toLowerCase();
-    
-    var outputText = "unknown word";
-    var wordFound = false;
-    
-    for (var index = 0; wordFound === false && index < englishWords.length; index++) {
-        
-        if (englishWords[index] === givenWord) {
-            outputText = finnishWords[index];
-            wordFound = true;
-        }
-    } 
-    
-    document.getElementById("pOutput").innerHTML = outputText;
+let lottery = []
+while(lottery.length<7){
+    let random = Math.floor(Math.random() * 39) + 1;
+    if(duplicateChecker(lottery, random)==true){
+        lottery.push(random);
+    }
 }
+console.log(lottery);
+document.getElementById("pOutput").innerHTML=lottery.join(" ");
 
-// These arrays are created when the HTML page is loaded
-var englishWords = ["cat", "dog", "bird"];
-var finnishWords = ["kissa", "koira", "lintu"];
-
-// End
+function duplicateChecker(arr, dub){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]==dub){
+            return false;
+        }
+    }
+    return true;
+}
