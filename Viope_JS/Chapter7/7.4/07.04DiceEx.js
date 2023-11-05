61a46e78c714e2a504be2b83a1a6a2e3cbfbd7d1
Liisa Davydov <liisamuttonen@gmail.com>
2023-11-05 20:28:44 +0200

    Add ch.7 ex.4

--- Viope_JS/Chapter7/7.4/07.04DiceEx.js
+++ Viope_JS/Chapter7/7.4/07.04DiceEx.js
@@ -1,11 +1,10 @@
 function getRandomInteger(min, max) {
-    // Return randomizedNumber between 'min' and 'max'.
+    return randomizedNumber=Math.floor(Math.random() * (max - min) + min);
     
 }
 
 function rollTheDice() {
-    // 1. Call the function getRandomInteger(min, max) with parameter values 1 and 6, and assign the return value of the function to the variable.
-   
-    // 2. Insert the value of the variable diceValue into the HTML div element whose id is 'dice'.
+    diceValue=getRandomInteger(1, 6);
+    document.getElementById("dice").innerHTML=diceValue;
     
 }
