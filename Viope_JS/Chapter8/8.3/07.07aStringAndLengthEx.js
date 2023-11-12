c8941e75b9a8b159153f60d25e31e6c426964727
Liisa Davydov <liisamuttonen@gmail.com>
2023-11-12 21:11:54 +0200

    Modify README file

---
+++ Viope_JS/Chapter8/8.3/07.07aStringAndLengthEx.js
@@ -0,0 +1,15 @@
+function processName() {
+
+    // read value of name from the input field
+   
+
+    // show it with "ALL CAPS" using toUpperCase()
+   
+
+    // show it with  "all small letters" using toLowerCase()
+ 
+
+    //tell how many characters are there (length, includes also all spaces) 
+   
+   
+}
---
+++ Viope_JS/Chapter8/8.3/README.md
@@ -0,0 +1,7 @@
+# String and Length
+
+Make a program, that asks for a text string (line of text), and tells information about the text string.
+
+Sample output for shown input:
+
+![String_and_Length](./07.07a.png)
