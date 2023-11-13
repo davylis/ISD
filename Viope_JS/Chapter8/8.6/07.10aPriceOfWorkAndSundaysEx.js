4fcd17007fc2881a2897593d7edebc0cf7d650fc
Liisa Davydov <liisamuttonen@gmail.com>
2023-11-13 09:52:00 +0200

    Modify README file and add HTML and JS

---
+++ Viope_JS/Chapter8/8.6/07.10aPriceOfWorkAndSundaysEx.js
@@ -0,0 +1,37 @@
+
+
+function calculate() {
+
+    // Read date text from the input field
+
+    // If it is not Sunday  (Call isSunday() function with a date text as a parameter)
+        //Show price of the repair work per hour during the workdays
+    //otherwise			
+        //Show price of the repair work per hour on Sundays
+
+}
+
+ // The function gets the date text in the format "dd.MM.yyyy". 
+ // The function returns Boolean value true if the date is for a Sunday, otherwise false.
+function isSunday(dateText) {
+	
+	// Split the given dateText into day, month and year parts using the substr() method
+	
+
+	// Create a new Date object to a variable 
+	
+
+	// Get the day of the week with its getDay() method. Sunday is number 0 .
+	
+
+	// If day of the week is Sunday 
+	
+		// return Boolean value true
+		
+	// otherwise	
+	
+       // return Boolean value false		
+	
+}
+
+
---
+++ Viope_JS/Chapter8/8.6/README.md
@@ -0,0 +1,9 @@
+# Price of Work and Sundays
+
+A construction firm is doing small repairs. Make a program for inputting the repair work details and calculating its price. The price for an hour of work is 48 euros during the workdays and 72 euros on Sundays. Define there the following function: isSunday(dateText) . The function will be called in function calculate().
+
+Sample output for shown input:
+
+![Repair_work](./07.10a.png)
+
+![Repair_work2](./07.10a_b.png)
