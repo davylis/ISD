d27ccd9450882d3a9031ed0275336ca9a17a62d6
Liisa Davydov <liisamuttonen@gmail.com>
2023-11-23 00:51:31 +0200

    Add Exam directory

---
+++ ExamPreparation/prep.js
@@ -0,0 +1,18 @@
+1.
+    Select one from the list below. Answer: _________
+    At home, the thing that reminds an algorithm the most would be:
+    a. electronic washing machine
+    b. housewarming party
+    c. remote control of TV or radio
+    d. computer game
+    e. programmable microwave oven
+    f. food recipe with ingredients and steps
+    g. programmable electronic calculator
+    h. none of the above
+
+Answer: f
+A food recipe typically consists of a set of instructions (steps) to be followed in a specific sequence to achieve a desired outcome (dish), much like an algorithm specifies a sequence of steps to perform a task or solve a problem. 
+
+2.
+
+
